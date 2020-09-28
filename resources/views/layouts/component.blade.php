eb94fb1019f64a26744d9e0254f6efd7f7c0c0bc
Jose Alejandro Alvarez Mendoza <alvarez.josealejandro3@gmail.com>
2020-09-28 17:53:17 -0300

    Refactoring JS

--- resources/views/layouts/component.blade.php
+++ resources/views/layouts/component.blade.php
@@ -82,42 +82,48 @@
 
     {{-- @TODO: move to a js file. --}}
     <script type="text/javascript">
-        var openmodal = document.querySelectorAll('.modal-open')
+        var openmodal = document.querySelectorAll('.modal-open');
+
         for (var i = 0; i < openmodal.length; i++) {
             openmodal[i].addEventListener('click', function(event){
-                event.preventDefault()
-                toggleModal()
+                event.preventDefault();
+                toggleModal();
             })
         }
 
-        const overlay = document.querySelector('.modal-overlay')
-        overlay.addEventListener('click', toggleModal)
+        const overlay = document.querySelector('.modal-overlay');
 
-        var closemodal = document.querySelectorAll('.modal-close')
+        if (overlay) {
+            overlay.addEventListener('click', toggleModal)
+        }
+
+        var closemodal = document.querySelectorAll('.modal-close');
+
         for (var i = 0; i < closemodal.length; i++) {
-            closemodal[i].addEventListener('click', toggleModal)
+            closemodal[i].addEventListener('click', toggleModal);
         }
 
         document.onkeydown = function(evt) {
-            evt = evt || window.event
-            var isEscape = false
+            evt = evt || window.event;
+            var isEscape = false;
+
             if ("key" in evt) {
-                isEscape = (evt.key === "Escape" || evt.key === "Esc")
+                isEscape = (evt.key === "Escape" || evt.key === "Esc");
             } else {
-                isEscape = (evt.keyCode === 27)
+                isEscape = (evt.keyCode === 27);
             }
             if (isEscape && document.body.classList.contains('modal-active')) {
-                toggleModal()
+                toggleModal();
             }
         };
 
-
         function toggleModal () {
-            const body = document.querySelector('body')
-            const modal = document.querySelector('.modal')
-            modal.classList.toggle('opacity-0')
-            modal.classList.toggle('pointer-events-none')
-            body.classList.toggle('modal-active')
+            const body = document.querySelector('body');
+            const modal = document.querySelector('.modal');
+
+            modal.classList.toggle('opacity-0');
+            modal.classList.toggle('pointer-events-none');
+            body.classList.toggle('modal-active');
         }
     </script>
 </body>

abb6ab6735921a1fd601773456c7795e28cf9e94
Jose Alejandro Alvarez Mendoza <alvarez.josealejandro3@gmail.com>
2020-10-01 08:30:56 -0300

    PSV-1: Making resposible search an add button elements

--- resources/views/livewire/products.blade.php
+++ resources/views/livewire/products.blade.php
@@ -1,28 +1,40 @@
 <div>
-    <div class="mb-4 flex">
-        <input id="product_search"
-               class="flex-1 bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 m-2 block appearance-none leading-normal"
-               type="text"
-               placeholder="{{ __("Search product") }}"
-               wire:model="search">
-        <button class="flex-3 bg-purple-600 hover:bg-purple-800 text-white font-bold py-2 px-4 rounded m-2 inline-flex items-center"
-                wire:click="redirectToProductForm">
-            <svg class="fill-current w-4 h-4 mr-2"
-                 xmlns="http://www.w3.org/2000/svg"
-                 viewBox="0 0 20 20"
-                 fill="currentColor">
-                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
-            </svg>
-            {{ __("Add Product") }}
-        </button>
+    <div class="w-full sm:max-w-full bg-white shadow-md overflow-hidden sm:rounded-lg bg-gray-100">
+        <div class="flex flex-col sm:flex-row items-center text-center">
+            <div class="flex flex-auto flex-shrink-0 mb-5 sm:mb-0">
+                <div class="relative mt-5 sm:m-auto w-auto">
+                    <div class="pointer-events-none absolute inset-y-0 left-0 pl-4 flex items-center">
+                        <svg class="fill-current pointer-events-none text-gray-600 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path></svg>
+                    </div>
+                    <input id="product_search"
+                           class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal pr-4 pl-10"
+                           type="text"
+                           placeholder="{{ __("Search product") }}"
+                           wire:model="search">
+                </div>
+            </div>
+            <div class="flex flex-auto">
+                <button class="bg-purple-600 hover:bg-purple-800 text-white font-bold w-full sm:w-auto py-2 px-4 mb-5 sm:m-auto rounded m-2 inline-flex items-center"
+                        wire:click="redirectToProductForm">
+                    <svg class="fill-current w-4 h-4 mr-2"
+                         xmlns="http://www.w3.org/2000/svg"
+                         viewBox="0 0 20 20"
+                         fill="currentColor">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
+                    </svg>
+                    {{ __("Add Product") }}
+                </button>
+            </div>
+        </div>
     </div>
+
 
     @if(count($products->items()) > 0)
     <div class="grid grid-cols-3 place-items-auto">
         @foreach($products as $product)
             <div class="m-2 max-w-xs rounded overflow-hidden shadow-lg">
                 <button wire:click="redirectToProductDetail({{ $product->id }})">
-                    <img class="w-64 h-64 object-cover"
+                    <img class="w-64 h-64 rounded-lg"
                          src="{{ get_s3_image($product->image) }}"
                          alt="{{ $product->sku }}"
                          title="{{ $product->sku }}">
